6f2d7660cc23a50a6935098945dcdbe0dda04516
Shahvalad <shahvaladayvazov@gmail.com>
2023-04-12 12:36:47 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿ namespace ConsoleApp28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Celsius celsius= new Celsius(70);
+            Kelvin kelvin= celsius;
+            Console.WriteLine(kelvin.kelvinDegree);
+
+        }
+    }
+   
+    }
+internal class Celsius
+{ public int Degree { get; set; }
+    public Celsius(int degree) 
+    {
+             Degree = degree;
+    }
+    //public static implicit operator Celsius(Kelvin kelvin)
+    //{
+    //    return new Celsius(kelvin.kelvinDegree - 273);
+    //}
+    }
+internal class Kelvin
+{
+        public int kelvinDegree { get; set; }
+        public Kelvin(int degree) 
+        {
+            kelvinDegree= degree;
+        }
+        public static implicit operator Kelvin(Celsius celsius)
+        {
+        return new Kelvin(celsius.Degree + 273);
+        }
+ }
+  
